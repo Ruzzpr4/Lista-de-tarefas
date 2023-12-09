1608c10194d98d87e3ee95b1370427720fe06281
Ruzzpr4 <raulstefani3333@gmail.com>
2023-12-09 01:30:24 -0300

    Aprendendo sobre Express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/json", (req, res) => {
+  // res.send("<h1>Minha lista de tarefas!</h1>");
+  res.json({ title: "Tarefa X", done: true });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor Iniciado!");
+});
