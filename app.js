cf6519a8788c5ce23e5c184d2e77b2b2b905ac70
Ruzzpr4 <146672538+Ruzzpr4@users.noreply.github.com>
2023-12-10 06:16:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/json", (req, res) => {
-  // res.send("<h1>Minha lista de tarefas!</h1>");
+  // res.send("<h1>Minha lista de tarefas</h1>");
   res.json({ title: "Tarefa X", done: true });
 });
 
